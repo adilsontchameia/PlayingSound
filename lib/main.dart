f8af79d20012390ecb92565a14d23c3f3814ba27
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-15 22:15:43 +0100

    pausando som

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,13 +16,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //Instanciando o pacote que reproducao de sons
+  //Instanciando a classe
+  AudioPlayer audioPlayer = AudioPlayer();
   AudioCache audioCache = AudioCache();
+  //Verificando se e a primeira vez a ser executada
+  //
   //Executando o som
   _executar() async {
-    //Instanciando a classe
-    final player = AudioCache();
     //Pegando o arquivo desejado para reproduzir
-    player.play("fear.mp3");
+    audioPlayer.play("fear.mp3");
+  }
+
+  _pausar() async {
+    int resultado = await audioPlayer.pause();
   }
 
   @override
@@ -40,7 +47,9 @@ class _HomeState extends State<Home> {
             children: [
               GestureDetector(
                 child: Image.asset("images/executar.png"),
-                onTap: () {},
+                onTap: () {
+                  _executar();
+                },
               ),
               GestureDetector(
                 child: Image.asset("images/pausar.png"),
